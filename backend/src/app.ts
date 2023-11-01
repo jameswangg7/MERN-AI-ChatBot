875ad5b2e2c117e0c846a7248e1aa3a2134c43b7
jameswangg7 <149620087+jameswangg7@users.noreply.github.com>
2023-11-01 19:40:33 +0000

    backend done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import express from 'express'
 //import {config} from 'dotenv'
 import morgan from 'morgan'
 import appRouter from './routes/index.js'
+import cookieParser from 'cookie-parser'
 
 //config()
 const app=express()
+app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(express.json())
 //remove it in prod
 app.use(morgan('dev'))
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,45 +1,81 @@
 import { NextFunction, Request, Response } from "express"
 import User from "../models/User.js"
 import { compare, hash } from "bcrypt"
+import { createToken } from "../utils/token-manager.js"
+import { COOKIE_NAME } from "../utils/constants.js"
 
-export const getAllUsers= async (req:Request,res: Response, next:NextFunction)=>{
+export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const users=await User.find()
-        return res.status(200).json({message: 'ok', users})
+        const users = await User.find()
+        return res.status(200).json({ message: 'ok', users })
     } catch (error) {
-        console.log(error);        
-        return res.status(500).json({message: 'error', cause: error.message})
-       
+        console.log(error);
+        return res.status(500).json({ message: 'error', cause: error.message })
+
     }
 }
-export const userSignup= async (req:Request,res: Response, next:NextFunction)=>{
+export const userSignup = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {name, email, password}=req.body
-        const existingUser= await User.findOne({email})
-        if(existingUser) return res.status(401).send("user already registered")
-        const hashedPassword=await hash(password, 10)
-        const user=new User({name,email,password: hashedPassword})
+        const { name, email, password } = req.body
+        const existingUser = await User.findOne({ email })
+        if (existingUser) return res.status(401).send("user already registered")
+        const hashedPassword = await hash(password, 10)
+        const user = new User({ name, email, password: hashedPassword })
         await user.save() //no...//newUser={name,email,password: hashedPassword}; const user= await User.create(newUser) 
-        return res.status(201).json({message: 'ok', id:user._id.toString()})
+       // create token and store cookie
+       res.clearCookie(COOKIE_NAME, {
+        path: "/",
+        domain: "localhost",
+        httpOnly: true,
+        signed: true,
+    })
+    const token = createToken(user._id.toString(), user.email, "7d")
+    const expires = new Date()
+    expires.setDate(expires.getDate() + 7)
+    res.cookie(COOKIE_NAME, token, {
+        path: "/",
+        domain: "localhost",
+        expires, httpOnly: true,
+        signed: true,
+    })
+
+        return res.status(201).json({ message: 'ok', id: user._id.toString() })
     } catch (error) {
-        console.log(error);        
-        return res.status(500).json({message: 'error', cause: error.message})       
+        console.log(error);
+        return res.status(500).json({ message: 'error', cause: error.message })
     }
 }
-export const userLogin= async (req:Request,res: Response, next:NextFunction)=>{
+export const userLogin = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {email, password}=req.body
-        const user= await User.findOne({email})
-        if(!user){
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if (!user) {
             return res.status(401).send("user not registered")
         }
-        const isPasswordCorrect= await compare(password, user.password)
-        if(!isPasswordCorrect){
+        const isPasswordCorrect = await compare(password, user.password)
+        if (!isPasswordCorrect) {
             return res.status(403).send("Incoorect Password")
         }
-        return res.status(200).json({message: 'ok', id:user._id.toString()})
+        //
+        res.clearCookie(COOKIE_NAME, {
+            path: "/",
+            domain: "localhost",
+            httpOnly: true,
+            signed: true,
+        })
+        const token = createToken(user._id.toString(), user.email, "7d")
+        const expires = new Date()
+        expires.setDate(expires.getDate() + 7)
+        res.cookie(COOKIE_NAME, token, {
+            path: "/",
+            domain: "localhost",
+            expires, httpOnly: true,
+            signed: true,
+        })
+
+        return res.status(200).json({ message: 'ok', id: user._id.toString() })
     } catch (error) {
-        console.log(error);        
-        return res.status(500).json({message: 'error', cause: error.message})       
+        console.log(error);
+        return res.status(500).json({ message: 'error', cause: error.message })
     }
 }
